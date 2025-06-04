10f7a42a30efbcd21440fbc43069f9a824b4b998
Ricardo Martinez <ricardo.a.martinez@gmail.com>
2025-06-04 16:15:11 -0500

    Update LeanVAE init file for real-time implementation compatibility

--- LeanVAE/__init__.py
+++ LeanVAE/__init__.py
@@ -1,4 +1,3 @@
-from .data import VideoData
+# from .data import VideoData  # Skip this import to avoid pytorch_lightning issues
 from .models.autoencoder import LeanVAE
-from .models.autoencoder_pl import AutoEncoderEngine 
-
+# from .models.autoencoder_pl import AutoEncoderEngine  # Skip this import too
